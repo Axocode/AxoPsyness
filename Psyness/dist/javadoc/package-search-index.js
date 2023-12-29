55fda131472b7375fe7219716d9e3ccf3726ca1b
Fernandoelcrack <yayferyayfer9@gmail.com>
2023-12-29 07:31:57 +0000

    1

--- Psyness/dist/javadoc/package-search-index.js
+++ Psyness/dist/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.axocode.dao"},{"l":"org.axocode.dao.service"},{"l":"org.axocode.helper"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"bm.java.ws"},{"l":"bm.java.ws.model"},{"l":"org.axocode.dao"},{"l":"org.axocode.dao.service"},{"l":"org.axocode.helper"},{"l":"org.axocode.model"},{"l":"org.bm.dao.service"}];updateSearchResults();
